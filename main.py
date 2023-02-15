81bb6706e2a622d50bac20ac69c0282a0d3b83fb
Ryan-Caruth <ryan.caruth2@gmail.com>
2023-02-14 18:51:36 -0700

    testing URL

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ class LoadSHP:
         self.iface = iface
 
     def load_layer(self):
-        self.iface.addVectorLayer('C:/Users/Ryan/Documents/programming/data_for_good/calgary_emergency_response_times'
-                                  '/data/postal_codes/alberta.shp', 'alberta', 'ogr')
+        self.iface.addVectorLayer('/data/postal_codes/alberta.shp', 'alberta', 'ogr')
 
     
