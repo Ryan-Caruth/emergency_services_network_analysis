9d8a84ace2628d7905fef20d8959d82d580ed58f
Ryan-Caruth <ryan.caruth2@gmail.com>
2023-02-14 18:21:06 -0700

    Adding alberta shapefile to qgis map

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Welcome to a network_analysis project')
+
+
+class LoadSHP:
+
+    def __init__(self, iface):
+        self.iface = iface
+
+    def load_layer(self):
+        self.iface.addVectorLayer('/data/postal_codes/alberta.shp', 'alberta', 'ogr')
+
+
+# def run_script(iface):
+#     fs = LoadSHP(iface)
+#     fs.load_layer()
+    
