cab2089b10460fd611d8a687eea21d1d1116d449
Ryan-Caruth <ryan.caruth2@gmail.com>
2023-02-15 21:38:41 -0700

    Changing the layer order which the shapefiles appear

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ class LoadSHP:
     def load_layer(self):
         self.iface.addVectorLayer(shapefiles.alberta_shp, 'alberta', 'ogr')
         self.iface.addVectorLayer(shapefiles.city_boundary_shp, 'calgary_boundary', 'ogr')
+        self.iface.addVectorLayer(shapefiles.roads_shp, 'calgary_roads', 'ogr')
+        self.iface.addVectorLayer(shapefiles.ems_stations_shp, 'calgary_ems_stations', 'ogr')
+        self.iface.addVectorLayer(shapefiles.fire_stations_shp, 'calgary_fire_stations', 'ogr')
+        self.iface.addVectorLayer(shapefiles.addresses_shp, 'calgary_addresses', 'ogr')
 
     def load_clipped_layer(self):
         self.iface.addVectorLayer(shapefiles.clipped_to_calgary, "Calgary", "ogr")
@@ -37,7 +41,18 @@ def remove_vector_layers():
             project.removeMapLayer(layer)
 
 
+def change_layer_order():
+    rearrange_layer = project.mapLayersByName("Calgary")[0]
+    root_layer = project.layerTreeRoot()
+    rearrange_layer_1 = root_layer.findLayer(rearrange_layer.id())
+    clone = rearrange_layer_1.clone()
+    parent = rearrange_layer_1.parent()
+    parent.insertChildNode(5, clone)
+    parent.removeChildNode(rearrange_layer_1)
+
+
 remove_vector_layers()
+change_layer_order()
 
 
 
